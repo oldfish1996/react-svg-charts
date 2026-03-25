68f99a9b105fd46ca1c39bfb8f5b4a47a16d1b92
chenyansong <chenyansong.cys@bytedance.com>
2026-03-25 15:37:16 +0800

    chore: deploy storybook to GitHub Pages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,13 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(tsx|ts)', '../src/**/*.mdx'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-docs'],
   docs: { autodocs: false },
+  viteFinal: async (config) => {
+    const basePath = process.env.STORYBOOK_BASE_PATH;
+    return {
+      ...config,
+      base: basePath || config.base,
+    };
+  },
 };
 
 export default config;
